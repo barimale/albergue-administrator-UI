44a23956e65ea92562944cb710c17c5e4f9d5c11
barimale <mateusz.wolnica@gmail.com>
2022-01-31 23:57:58 +0100

    .

--- src/internali18n.ts
+++ src/internali18n.ts
@@ -2,18 +2,27 @@ import i18next from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+const loadPath = `${process.env.REACT_APP_ADMINISTRATOR_BACKEND_APP}/locales/{{lng}}/{{ns}}.json`;
+
 const internali18n = i18next.createInstance();
 
 internali18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    debug: true,
+    debug: process.env.NODE_ENV !== 'production',
     defaultNS: 'externals',
+    fallbackLng: 'en',
     backend: {
-      loadPath: '/locales/externals/{{lng}}.json',
-      addPath: '/locales/externals/{{lng}}.json',
-      reloadInterval: 10000,
+      loadPath,
+      addPath: loadPath,
+      crossDomain: true,
+      withCredentials: false,
+      requestOptions: {
+        // mode: 'no-cors',
+        mode: 'cors', credentials: 'same-origin', cache: 'default',
+      },
+      // reloadInterval: 10000,
     },
     react: {
       bindI18n: 'languageChanged',
